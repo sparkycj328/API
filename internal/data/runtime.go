f105be9bb07234e288850d4b15638bd02600f352
Carlos <sparkycj328@gmail.com>
2022-09-06 23:09:19 -0500

    Added custom structs and types

---
+++ internal/data/runtime.go
@@ -0,0 +1,17 @@
+package data
+
+import (
+	"fmt"
+	"strconv"
+)
+
+// Declare a custom Runtime type, which has the underlying type
+type Runtime int32
+
+func (r Runtime) MarshalJSON() ([]byte, error) {
+	jsonValue := fmt.Sprintf("%d mins", r)
+
+	quotedJSONValue := strconv.Quote(jsonValue)
+
+	return []byte(quotedJSONValue), nil
+}
